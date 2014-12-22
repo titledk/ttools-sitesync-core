465e86c0ef82b16c3c45013f0e35436cf166ed72
Anselm Christophersen <ac@anselm.dk>
2014-12-22 19:43:55 +0100

    started out on sync environments script

---
+++ local/sync-environments.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#This script syncs from a specified environment to a specified environment
+#Run like this: sync-environments.sh Live Dev
+#NOTE: Usually this should be called through "sync-environments.sh" from
+#sitesync-core
+
+#From
+if [ -z "${1}" ]; then
+	echo "Please specify \"from\" environment";
+	exit;
+fi
+FROM=$1
+
+#To
+if [ -z "${2}" ]; then
+	echo "Please specify \"to\" environment";
+	exit;
+fi
+TO=$2
+
+#Protection - disallowing syncing to live
+if [[ "$TO" == "Live" ]]; then
+	echo "You are not allowed to sync to Live. Exiting.";
+	echo "Note: if this is for your first deployment, uncomment this line.";
+	exit;
+fi
+
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../.. && pwd )";
+MODULEDIR="$BASEDIR/ttools-sitesync-wordpress";
+
+#getting configuration variables
+VARS="$BASEDIR/ttools-core/lib/vars.sh"
+eval `$VARS`
+
+
+#getting environment variables
+#FROM will be the base
+ENV=$FROM;
+ENVVARS="$BASEDIR/ttools-core/lib/vars-for-env.sh $ENV"
+eval `$ENVVARS`
+
+
+
+if [[ "$FROM" == "Local" ]]; then
+
+	#this is the basic local sync, which is implemented by each framework specific module
+	$BASEDIR/$ServerSync_FrameworkModule/lib/sync-current-site-with-env.sh to $TO
+	
+else
+
+	if [[ "$TO" == "Local" ]]; then
+		#this is the basic local sync, which is implemented by each framework specific module
+		$BASEDIR/$ServerSync_FrameworkModule/lib/sync-current-site-with-env.sh from $FROM
+	
+	else
+	
+		echo "not yet implemented...";
+		
+		#if the mode is not local we'll ssh into the (TODO which?)
+		#server and run the sync script
+
+	fi
+	
+	
+	
+fi
+
+
